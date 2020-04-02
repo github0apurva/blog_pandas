797de56761619541e9d15fdb607829b8649e730e
Apurva <apurva.c27@gmail.com>
2020-04-02 14:52:58 +0530

    made changes to pandas1.py for viewing head of data

--- pandas1.py
+++ pandas1.py
@@ -6,3 +6,4 @@ data_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/credit-scr
 wget.download(data_url,'crx.csv')
 crx_base = pd.read_csv("crx.csv", names = ["A1","A2","A3","A4","A5","A6","A7","A8","A9","A10","A11","A12","A13","A14","A15","A16"] )
 crx_base.replace('?', np.NaN, inplace = True)
+crx_base.head()
